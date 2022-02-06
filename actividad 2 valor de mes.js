13896f45ea25e0f7cd20c472003561b527de1147
Nicolas Marroquin <nicolasmarroquin1999@gmail.com>
2022-02-05 22:49:27 -0600

    first commit

---
+++ actividad 2 valor de mes.js
@@ -0,0 +1,32 @@
+var mes = 10
+
+mes = prompt("por favor ingrese mes ")
+
+if(mes == 12){
+	console.log("es invierno")
+}else if(mes == 1){
+	console.log("es invierno")
+}else if (mes == 2) {
+	console.log("es invierno")
+}else if (mes == 3){
+	console.log("es primavera")
+}else if (mes ==4){
+	console.log("es primavera")
+}else if (mes==5){
+	console.log("es primavera")
+}else if (mes ==6){
+	console.log("es verano")
+}else if (mes ==7){
+	console.log("es verano")
+}else if (mes ==8){
+	console.log("es verano")
+}else if ( mes ==9){
+	console.log("es otoño")
+}else if (mes == 10){
+	console.log("es otoño")
+}else if (mes ==11){
+	console.log("es otoño")
+}else if (mes >12){
+  console.log("no es mes ")
+}
+console.log("carnet:22002895 Jose Nicolas de Jesus Marroquin Solis")
